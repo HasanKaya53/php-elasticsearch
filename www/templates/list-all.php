70f990c44f16034e5bcbb94f3488737892f500b6
hasankaya53 <hasankayaaa53@gmail.com>
2024-04-10 21:52:43 +0300

    list all data

--- www/templates/list-all.php
+++ www/templates/list-all.php
@@ -1,11 +1,23 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+<!-- jqeuery. -->
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+
+
 <ul>
     <li><a href="/">Home</a></li>
     <li><a href="/list-all">List All</a></li>
-    <li><a href="/search">Search</a></li>
-</ul>
+</ul
 
+
+
+<!-- search.. -->
+<form >
+    <!-- label -->
+    <label for="name">Search: </label>
+    <input type="text" name="name" id="name" placeholder="Name">
+</form>
 
 
 
